67f3791fb36c713896f7ecd938357790443e5bfa
jhsware <sebastian@urbantalk.se>
2017-05-18 10:51:21 +0200

    Standard fixes

--- gulpTasks/webpack.server.config.js
+++ gulpTasks/webpack.server.config.js
@@ -6,12 +6,12 @@ module.exports = {
   },
   module: {
     loaders: [{
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        query: {
-          // babel-loader doesn't pick up the transform-decorators-legacy plugin setting from babelrc entry in package.json
-          plugins: ['transform-decorators-legacy' ]
-        }
+      test: /\.jsx$/,
+      loader: 'babel-loader',
+      query: {
+        // babel-loader doesn't pick up the transform-decorators-legacy plugin setting from babelrc entry in package.json
+        plugins: ['transform-decorators-legacy']
+      }
     }]
   },
   externals: Object.keys(require('../package.json').dependencies)
