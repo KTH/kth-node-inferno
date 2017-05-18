44a0a34df69ddbf7e78efa86d7877731915eebe3
jhsware <sebastian@urbantalk.se>
2017-05-18 10:50:47 +0200

    Standard fixes

--- gulpTasks/webpack.config.js
+++ gulpTasks/webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-	devtool: 'source-map',
-	module: {
-		loaders: [{
-			test: /\.jsx$/,
-			loader: 'babel-loader',
-			query: {
-				// babel-loader doesn't pick up the transform-decorators-legacy plugin setting from babelrc entry in package.json
-				plugins: ['transform-decorators-legacy' ]
-			}
-		}]
-	}
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      loader: 'babel-loader',
+      query: {
+        // babel-loader doesn't pick up the transform-decorators-legacy plugin setting from babelrc entry in package.json
+        plugins: ['transform-decorators-legacy' ]
+      }
+    }]
+  }
 }
